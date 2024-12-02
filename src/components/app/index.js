f50b1553ac556bfa4cc36ce5eeb25402c42aecf8
Joris Baum <joris@audriga.com>
2024-12-02 15:44:47 +0100

    Use libxml2-wasm in react

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -7,14 +7,22 @@ import { XmlDocument } from 'libxml2-wasm';
 // Or, utils, like callWtih. Refer to zm-x-web, zimbraManager/shims.js
 // More shims can be added here if necessary; also requires an update to zimlet-cli
 
-console.log("test"); // This is being logged
-const doc1 = XmlDocument.fromString('<note><to>Tove</to></note>');
-doc1.dispose();
-console.log(doc1); // This is also being logged
-
 @withIntl()
 export default class App extends Component {
 	render() {
+		const testLibxml2 = async () => {
+			try {
+				// Example: Parse a simple XML string
+				const xmlDoc = XmlDocument.fromString(`<root><child>Test</child></root>`);
+				const rootNode = xmlDoc.root;
+				setResult(rootNode ? rootNode.name : "No root node found");
+			} catch (e) {
+				console.error("Error using libxml2-wasm:", e);
+				setError(e.message || "Unknown error");
+			}
+		};
+
+		testLibxml2();
 		return (
 			<div class={style.wrapper}>
 				<div class={style.main}>Hello World</div>
